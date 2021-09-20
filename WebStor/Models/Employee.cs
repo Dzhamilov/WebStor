7b0bb15608c941b7c3e1127bd0662120e4c3420a
Dzhamilov <akbarshoh-95@mail.ru>
2021-09-20 14:10:05 +0300

    модель сотрудника

---
+++ WebStor/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStor.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    //public record Employee2(int Id, string LastName, string FirstName, string Patronymic, int Age);
+}
