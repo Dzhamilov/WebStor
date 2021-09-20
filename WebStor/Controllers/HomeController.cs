03f79cc942923c0c10e8e30d755438c6403c771b
Dzhamilov <akbarshoh-95@mail.ru>
2021-09-20 14:10:58 +0300

    представление сотрудника

--- WebStor/Controllers/HomeController.cs
+++ WebStor/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStor.Models;
 
 namespace WebStor.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
         public IActionResult Index()
         {
             //return Content("Hello from first controller");
@@ -18,6 +25,11 @@ namespace WebStor.Controllers
         public IActionResult SecondAction(string id) 
         {
             return Content($"Second action with parameter {id}");
+        }
+
+        public IActionResult Employees() 
+        {
+            return View(_Employees);
         }
     }
 }
