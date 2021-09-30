d2b3c677fdede83df89e21b3a55c4e81a298fff6
Dzhamilov <akbarshoh-95@mail.ru>
2021-09-30 17:25:58 +0300

    CRUD-интерфейс в контроллере сотрудников

--- WebStor/Controllers/EmployeesController.cs
+++ WebStor/Controllers/EmployeesController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using WebStor.Data;
 using WebStor.Models;
 using WebStor.Services.Interfaces;
+using WebStor.ViewModels;
 
 namespace WebStor.Controllers
 {
@@ -39,6 +40,86 @@ namespace WebStor.Controllers
             return View(employee);
         }
 
+        public IActionResult Create() => View("Edit", new EmployeeViewModel());
+
+        #region Edit
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+                return View(new EmployeeViewModel());
+
+            var employee = _EmployeesData.GetById((int)id);
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Name = employee.FirstName,
+                LastName = employee.LastName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            if (model.LastName == "Асама" && model.Name == "Бин" && model.Patronymic == "Ладан")
+                ModelState.AddModelError("", "Террористов не берём!");
+
+            if (!ModelState.IsValid) return View(model);
+
+            var employee = new Employee
+            {
+                Id = model.Id,
+                LastName = model.LastName,
+                FirstName = model.Name,
+                Patronymic = model.Patronymic,
+                Age = model.Age,
+            };
+
+            if (employee.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Update(employee);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region Delete
+
+        public IActionResult Delete(int id)
+        {
+            if (id < 0) return BadRequest();
+
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                Name = employee.FirstName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _EmployeesData.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
 
     }
 }
