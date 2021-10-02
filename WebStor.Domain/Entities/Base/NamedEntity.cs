087ac6f5f860152ec0dab41b69c39648658fe449
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-02 21:00:32 +0300

    Сущности бренда и секции

---
+++ WebStor.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebStor.Domain.Entities.Base.Interfaces;
+
+namespace WebStor.Domain.Entities.Base
+{
+    public abstract class NamedEntity : Entity, INamedEntity
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
