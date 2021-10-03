1c181b610d2e7878aa562421e3e56fb6d67d98c0
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-03 22:38:54 +0300

    Сервис каталога товаров + набор тестовых данных

---
+++ WebStor/Services/InMemoryProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStor.Data;
+using WebStor.Domain.Entities;
+using WebStor.Services.Interfaces;
+
+namespace WebStor.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        IEnumerable<Section> IProductData.GetSections() => TestData.Sections;
+
+        IEnumerable<Brand> IProductData.GetBrands() => TestData.Brands;
+    }
+}
---
+++ WebStor/Services/Interfaces/IProductData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStor.Domain.Entities;
+
+namespace WebStor.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
