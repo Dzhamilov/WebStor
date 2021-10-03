d2523daa3396efe5d103e121fdb72f7f48a85ffa
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-03 23:09:17 +0300

    Заготовки визуальных компонент

---
+++ WebStor/Components/BrandsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStor.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+
+        public IViewComponentResult Invoke() => View();
+    }
+}
