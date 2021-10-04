04df8ca730cedc32e9b8a9b6fd36f3ef6ee6d06a
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-04 17:50:58 +0300

    Логика визуального компонента брендов

--- WebStor/Components/BrandsViewComponent.cs
+++ WebStor/Components/BrandsViewComponent.cs
@@ -3,13 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStor.Services.Interfaces;
+using WebStor.ViewModels;
 
 namespace WebStor.Components
 {
+    //[ViewComponent(Name = "BrandsView")]
     public class BrandsViewComponent : ViewComponent
     {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
         //public async Task<IViewComponentResult> InvokeAsync() => View();
 
-        public IViewComponentResult Invoke() => View();
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+               .OrderBy(b => b.Order)
+               .Select(b => new BrandViewModel
+               {
+                   Id = b.Id,
+                   Name = b.Name,
+               });
     }
 }
