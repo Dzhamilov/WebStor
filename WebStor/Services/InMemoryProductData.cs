49af973a672e4be28aeab71d1e5150684ec21659
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-04 18:30:20 +0300

    Контроллер каталога товаров

--- WebStor/Services/InMemoryProductData.cs
+++ WebStor/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStor.Data;
+using WebStor.Domain;
 using WebStor.Domain.Entities;
 using WebStor.Services.Interfaces;
 
@@ -13,5 +14,21 @@ namespace WebStor.Services
         IEnumerable<Section> IProductData.GetSections() => TestData.Sections;
 
         IEnumerable<Brand> IProductData.GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IEnumerable<Product> query = TestData.Products;
+
+            //if (Filter?.SectionId != null)
+            //    query = query.Where(p => p.SectionId == Filter.SectionId);
+
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(p => p.SectionId == section_id);
+
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(p => p.BrandId == brand_id);
+
+            return query;
+        }
     }
 }
--- WebStor/Services/Interfaces/IProductData.cs
+++ WebStor/Services/Interfaces/IProductData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStor.Domain;
 using WebStor.Domain.Entities;
 
 namespace WebStor.Services.Interfaces
@@ -11,5 +12,7 @@ namespace WebStor.Services.Interfaces
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
     }
 }
