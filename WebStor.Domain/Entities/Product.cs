f2de68abf490307387b0ba119cb23040d8d3e324
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-04 17:57:25 +0300

    Определение товара

---
+++ WebStor.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStor.Domain.Entities.Base;
+using WebStor.Domain.Entities.Base.Interfaces;
+
+namespace WebStor.Domain.Entities
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int SectionId { get; set; }
+
+        public Section Section { get; set; }
+
+        public int? BrandId { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
