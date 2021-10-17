fd101b0a099cd9ff7e6245174da2eb04a01e9cc3
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-17 10:39:32 +0300

    Заготовка сервиса корзины

---
+++ WebStor/Services/InCookies/InCookiesCartService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStor.Domain.Entities;
+using WebStor.Services.Interfaces;
+using WebStor.ViewModels;
+
+namespace WebStor.Services.InCookies
+{
+    public class InCookiesCartService : ICartService
+    {
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        private readonly IProductData _ProductData;
+        private readonly string _CartName;
+
+        
+
+        public InCookiesCartService(IHttpContextAccessor HttpContextAccessor, IProductData ProductData)
+        {
+            _HttpContextAccessor = HttpContextAccessor;
+            _ProductData = ProductData;
+
+            var user = HttpContextAccessor.HttpContext!.User;
+            var user_name = user.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
+
+            _CartName = $"GB.WebStor.Cart{user_name}";
+        }
+
+        public void Add(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Decrement(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
