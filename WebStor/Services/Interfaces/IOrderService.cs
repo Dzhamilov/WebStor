6a718de73452b5b163a52638800ce8230a5fe471
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-24 14:53:33 +0300

    Модели заказа и элемента заказа

---
+++ WebStor/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStor.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetUserOrders(string UserName);
+
+        Task<Order> GetOrderById(int id);
+
+        Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel);
+    }
+}
