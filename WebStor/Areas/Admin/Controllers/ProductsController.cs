8f5397a02e0bee8355e8fe59526e3d4db1a2ec15
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-24 14:31:04 +0300

    Заготовка контроллера управления списком товаров

---
+++ WebStor/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStor.Domain.Entities.Base.Identity;
+using WebStor.Services.Interfaces;
+
+namespace WebStor.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = Role.Administrators)]
+    public class ProductsController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index()
+        {
+            var products = _ProductData.GetProducts();
+            return View(products);
+        }
+
+        public IActionResult Edit(int id) => RedirectToAction(nameof(Index));
+
+        public IActionResult Delete(int id) => RedirectToAction(nameof(Index));
+    }
+}
