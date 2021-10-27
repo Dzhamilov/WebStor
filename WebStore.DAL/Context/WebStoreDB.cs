bba62eef3fa813aa9800eab074dadcb079721b9b
Dzhamilov <akbarshoh-95@mail.ru>
2021-10-27 07:37:16 +0300

    Миграция БД для Заказов

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebStor.Domain.Entities;
 using WebStor.Domain.Entities.Base.Identity;
+using WebStor.Domain.Entities.Orders;
 
 namespace WebStore.DAL.Context
 {
